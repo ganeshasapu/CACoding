8e45790c94a8fe1f20dd8bd0ce28037549316622
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-10-14 14:15:47 -0400

    Created clearUsers use case

--- src/app/SignupUseCaseFactory.java
+++ src/app/SignupUseCaseFactory.java
@@ -1,9 +1,15 @@
 package app;
 
+import interface_adapter.clear_users.ClearController;
+import interface_adapter.clear_users.ClearPresenter;
 import interface_adapter.login.LoginViewModel;
 import interface_adapter.signup.SignupController;
 import interface_adapter.signup.SignupPresenter;
 import interface_adapter.signup.SignupViewModel;
+import use_case.clear_users.ClearInputBoundary;
+import use_case.clear_users.ClearInteractor;
+import use_case.clear_users.ClearOutputBoundary;
+import use_case.clear_users.ClearUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
 import entity.CommonUserFactory;
 import entity.UserFactory;
@@ -22,11 +28,14 @@ public class SignupUseCaseFactory {
     private SignupUseCaseFactory() {}
 
     public static SignupView create(
-            ViewManagerModel viewManagerModel, LoginViewModel loginViewModel, SignupViewModel signupViewModel, SignupUserDataAccessInterface userDataAccessObject) {
+            ViewManagerModel viewManagerModel, LoginViewModel loginViewModel, SignupViewModel signupViewModel, SignupUserDataAccessInterface userDataAccessObject, ClearUserDataAccessInterface clearUserDataAccessInterface ){
 
         try {
             SignupController signupController = createUserSignupUseCase(viewManagerModel, signupViewModel, loginViewModel, userDataAccessObject);
-            return new SignupView(signupController, signupViewModel);
+            ClearPresenter clearPresenter = new ClearPresenter();
+            ClearInputBoundary clearUsersUseCaseInteractor = new ClearInteractor(clearUserDataAccessInterface, clearPresenter);
+            ClearController clearController = new ClearController(clearUsersUseCaseInteractor);
+            return new SignupView(signupController, clearController, signupViewModel);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file.");
         }
